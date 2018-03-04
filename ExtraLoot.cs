29f8de0561d3fd946b0c023a8a945a75133e0a90
salva <salvadj1@hotmail.com>
2018-03-04 20:36:31 +0100

    Publish 1.0

---
+++ ExtraLoot.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Fougerite;
+using UnityEngine;
+using System.IO;
+
+using System.Collections;
+namespace ExtraLoot
+{
+    public class ExtraLoot : Fougerite.Module
+    {
+        public override string Name { get { return "ExtraLoot"; } }
+        public override string Author { get { return "Salva/juli"; } }
+        public override string Description { get { return "ExtraLoot"; } }
+        public override Version Version { get { return new Version("1.0"); } }
+
+        public Loot LootClass;
+        public GameObject LootClassLoad;
+
+        public static System.IO.StreamWriter file;
+        public static string rutafileloot = "";
+        public static List<string> AreasDeLoot = new List<string>();
+
+        public override void Initialize()
+        {
+            if (!File.Exists(Path.Combine(ModuleFolder, "Loot.txt"))) { File.Create(Path.Combine(ModuleFolder, "Loot.txt")).Dispose(); }
+            rutafileloot = Path.Combine(ModuleFolder, "Loot.txt");
+            ReloadLoot();
+            Hooks.OnServerInit += OnServerInit;
+            Hooks.OnCommand += OnCommand;
+        }
+        public override void DeInitialize()
+        {
+            if (LootClassLoad != null) UnityEngine.Object.DestroyImmediate(LootClassLoad);
+            Hooks.OnServerInit -= OnServerInit;
+            Hooks.OnCommand -= OnCommand;
+        }
+        public void OnServerInit()
+        {
+            LootClassLoad = new GameObject();
+            LootClass = LootClassLoad.AddComponent<Loot>();
+            UnityEngine.Object.DontDestroyOnLoad(LootClassLoad);
+        }
+
+        public void OnCommand(Fougerite.Player player, string cmd, string[] args)
+        {
+            if (cmd == "addloot")
+            {
+                if (!player.Admin) { return; }
+                string line = player.Location.ToString();
+                file = new System.IO.StreamWriter(rutafileloot, true);
+                file.WriteLine(line);
+                file.Close();
+                player.Message("Saved!");
+            }
+            if (cmd == "spawnloot")
+            {
+                if (!player.Admin) { return; }
+                foreach (string linea in File.ReadAllLines(rutafileloot))
+                {
+                    if (!linea.Contains("("))
+                    {
+                        continue;
+                    }
+                    var posfinal = Util.GetUtil().ConvertStringToVector3(linea);
+                    World.GetWorld().Spawn("BoxLoot", posfinal);
+                }
+            }
+            if (cmd == "clearloot")
+            {
+                if (!player.Admin) { return; }
+                foreach (string linea in File.ReadAllLines(rutafileloot))
+                {
+                    if (!linea.Contains("("))
+                    {
+                        continue;
+                    }
+                    var posfinal = Util.GetUtil().ConvertStringToVector3(linea);
+                    var obj = Physics.OverlapSphere(posfinal, 5);
+                    foreach (var xs in obj)
+                    {
+                        if (xs.name.ToLower().Contains("box"))
+                        {
+                            Util.GetUtil().DestroyObject(xs.gameObject);
+                        }
+                    }
+                }
+                player.Message("Loot Clean!");
+            }
+            if (cmd == "reloadloot")
+            {
+                if (!player.Admin) { return; }
+                ReloadLoot();
+                player.Message("Loot Reloaded");
+            }
+        }
+
+        public static void ReloadLoot()
+        {
+            //borra el listado actual
+            AreasDeLoot.Clear();
+            foreach (string linea in File.ReadAllLines(rutafileloot))
+            {
+                if (!linea.Contains("("))
+                {
+                    continue;
+                }
+                AreasDeLoot.Add(linea);
+            }
+            return;
+        }
+
+    }
+}
---
+++ Loot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using System.Collections;
+using Fougerite;
+
+namespace ExtraLoot
+{
+    public class Loot : MonoBehaviour
+    {
+        public static float AjusteEjeY = 1.6f;
+        
+        void Start()
+        {
+            StartCoroutine(ZonasExtraLoot());
+        }
+        IEnumerator ZonasExtraLoot()
+        {
+            //ESPARER ANTES COMENZAR
+            yield return new WaitForSeconds(120); // 20mins * 60 = 1800
+
+            Logger.Log("Adding Loot ----- Start Loop IEnumerator");
+            foreach (var x in ExtraLoot.AreasDeLoot)
+            {
+                var pos = Util.GetUtil().ConvertStringToVector3(x);
+                var posfinal = new Vector3(pos.x, pos.y - AjusteEjeY, pos.z);
+                //Borrar caja para evitar que se superpongan
+                var obj = Physics.OverlapSphere(posfinal, 2);
+                foreach (var xs in obj)
+                {
+                    if (xs.name.ToLower().Contains("box"))
+                    {
+                        Util.GetUtil().DestroyObject(xs.gameObject);
+                    }
+                }
+                //Poner Caja Neva
+                yield return new WaitForSeconds(1);
+                World.GetWorld().Spawn("BoxLoot", posfinal);
+            }
+
+            Logger.Log("Adding Loot ----- END");
+            //ESPARER ANTES COMENZAR REPETIR
+            yield return new WaitForSeconds(1800); // 20mins * 60 = 1800
+            //REPETIR RUTINA
+            StartCoroutine(ZonasExtraLoot());
+        }
+    }
+}
